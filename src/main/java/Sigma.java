3317de77969223b7c8173be9b0670e412ef258e9
heehaw1234 <seanlee2577@gmail.com>
2026-01-29 18:06:51 +0800

    changed name to sigma

---
+++ src/main/java/Sigma.java
@@ -0,0 +1,15 @@
+public class Sigma {
+    public static void main(String[] args) {
+        String logo = " ____  ___  ____  __  __    _    \n"
+                + "/ ___||_ _|/ ___||  \\/  |  / \\   \n"
+                + "\\___ \\ | || |  _ | |\\/| | / _ \\  \n"
+                + " ___) || || |_| || |  | |/ ___ \\ \n"
+                + "|____/|___|\\____||_|  |_/_/   \\_\\\n";
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello I'm\n" + logo);
+        System.out.println("What can i do for you");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
