a3325ec32dd6e8e467ee18e5053a4e461f59ebfe
heehaw1234 <seanlee2577@gmail.com>
2026-01-29 18:38:42 +0800

    added documentation, comments; renamed methods and variables

--- src/main/java/Sigma.java
+++ src/main/java/Sigma.java
@@ -1,10 +1,14 @@
-import java.util.Scanner;
+import java.util.Scanner; // object to take in user input
 
 public class Sigma {
-    // attributes
-    private static Task[] taskList = new Task[100]; // initialise an array of size 100
+    /** size of the list of tasks */
+    private static final int LIST_SIZE = 100;
+    /** list of tasks added */
+    private static Task[] taskList = new Task[LIST_SIZE]; // initialise an array of size 100
+    /** head of the taskList */
     private static int taskListHead = 0;
 
+    /** driver code, entry point of programme */
     public static void main(String[] args) {
         sayIntro();
 
@@ -13,70 +17,78 @@ public class Sigma {
 
         while (true){
             String userInput = in.nextLine();
-            if (userInput.equals("bye")){
+            if (userInput.equals("bye")) {
                 sayBye();
                 return;
             }
-            if (userInput.equals("list")){
+            if (userInput.equals("list")) {
                 printList();
                 continue;
             }
-            if (userInput.startsWith("mark")){
-                marker(userInput.substring(5, 6), true);
+            if (userInput.startsWith("mark")) {
+                setTaskStatus(userInput.substring(5, 6), true);
                 continue;
             }
-            if (userInput.startsWith("unmark")){
-                marker(userInput.substring(7, 8), false);
+            if (userInput.startsWith("unmark")) {
+                setTaskStatus(userInput.substring(7, 8), false);
                 continue;
             }
 
-            if (userInput.equals("sigma")){
+            if (userInput.equals("sigma")) {
                 echo("SIGMA INDEED!!!!");
             }
-            addList(userInput);
+            addTask(userInput);
         }
     }
 
-    private static void marker(String index, boolean isMark){
+    /** this function sets whether or not a task is marked */
+    private static void setTaskStatus(String index, boolean isMark) {
         int targetIndex = Integer.parseInt(index);
         String isDoneString = (isMark) ? "Nice! I've marked this task as done:" : "OK, I've marked this task as not done yet:";
-        if (isMark){
+
+        if (isMark) {
             taskList[targetIndex - 1].markAsDone();
         }
-        else if (!isMark){
+        else if (!isMark) {
             taskList[targetIndex - 1].markAsNotDone();
         }
+
         System.out.println("____________________________________________________________");
         System.out.println(isDoneString);
         System.out.println(taskList[targetIndex - 1].returnTaskString());
         System.out.println("____________________________________________________________");
     }
 
-    private static void sayIntro(){
+    /** this function greets the user */
+    private static void sayIntro() {
         String logo = " ____  ___  ____  __  __    _    \n"
                 + "/ ___||_ _|/ ___||  \\/  |  / \\   \n"
                 + "\\___ \\ | || |  _ | |\\/| | / _ \\  \n"
                 + " ___) || || |_| || |  | |/ ___ \\ \n"
                 + "|____/|___|\\____||_|  |_/_/   \\_\\\n";
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello I'm\n" + logo);
         System.out.println("What can i do for you?\n");
         System.out.println("____________________________________________________________");
     }
 
-    private static void echo(String userInput){
+    /** this function echoes back what the user input*/
+    private static void echo(String userInput) {
         System.out.println("____________________________________________________________");
         System.out.println(userInput);
         System.out.println("____________________________________________________________");
     }
 
-    private static void sayBye(){
+    /** this method says goodbye to the user */
+    private static void sayBye() {
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
 
-    private static void addList(String userInput){
+    /** this function adds a task to the list of tasks */
+    private static void addTask(String userInput) {
         Task toAdd = new Task(userInput);
         taskList[taskListHead] = toAdd;
         taskListHead += 1;
@@ -85,10 +97,11 @@ public class Sigma {
         System.out.println("____________________________________________________________");
     }
 
-    private static void printList(){
+    /** this function prints the list of tasks a user has added with the checkbox status displayed */
+    private static void printList() {
         System.out.println("____________________________________________________________");
         System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < taskListHead; i += 1){
+        for (int i = 0; i < taskListHead; i += 1) {
             System.out.println((i+1) + "." + taskList[i].returnTaskString());
         }
         System.out.println("____________________________________________________________");
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,25 +1,32 @@
 public class Task {
+    /** description of the task itself */
     protected String description;
+    /** boolean flag to set if task has been marked 'X' or not */
     protected boolean isDone;
 
+    /** constructor to construct a new task */
     public Task(String description) {
         this.description = description;
-        this.isDone = false;
+        this.isDone = false; // defaults to undone
     }
 
+    /** returns the status of whether checkbox is marked or not */
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
-    public String returnTaskString(){
+    /** returns a string of the task with checkbox */
+    public String returnTaskString() {
         return "[" + getStatusIcon() + "] " + description;
     }
 
-    public void markAsDone(){
+    /** sets isDone to true */
+    public void markAsDone() {
         isDone = true;
     }
 
-    public void markAsNotDone(){
+    /** sets isDone to false */
+    public void markAsNotDone() {
         isDone = false;
     }
 }
